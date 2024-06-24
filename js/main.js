cdd7b270f5d6944cfc9b4ff83dae57711de3f92e
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-24 17:03:09 +0200

    feat: add base program

--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+// estrapolo il metodo di creazione app vue
+const { createApp } = Vue;
+
+// creazione un'istanza di un app vue e la colleghiamo al nostro tag di contenitore globale 
+createApp({
+    // tutti i dati dell'applicazione
+    data () {
+        return {
+            message: "Hello vue!!"
+        }
+    }
+}).mount('#app');
